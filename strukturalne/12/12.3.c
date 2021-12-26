7c1d174111a967a8143ffaf07121bee368014a15
Pomaranczarz <and.cza553@gmail.com>
2021-12-26 21:08:17 +0100

    dodano rozwiązanie do 12.3

--- strukturalne/12/12.3.c
+++ strukturalne/12/12.3.c
@@ -2,17 +2,34 @@
 #include <string.h>
 
 #define LINE_WIDTH 80
+#define MAX_LEN 320
 void PrintCentered(const char* str, unsigned length);
 
 int main(void) {
-
-
+    char str[MAX_LEN];
+    printf("Podaj napis: ");
+    fgets(str, MAX_LEN, stdin);
+    PrintCentered(str, strlen(str) - 1);
+    
     return 0;
 }
 
 void PrintCentered(const char* str, unsigned length) {
-    unsigned index = 0;
-    while (index != length) {
-        
+    char* str_array[length / LINE_WIDTH + 1];
+    int i = 0;
+    int j = 0;
+    while (i < length) {
+        str_array[j] = str + i;
+        i += LINE_WIDTH;
+        j++;
     }
+
+    for (int k = 0; k < j; k++) {
+        int spaces = (LINE_WIDTH - strlen(str_array[k])) / 2;
+        for (int l = 0; l < spaces; l++) {
+            printf(" ");
+        }
+        printf("%s\n", str_array[k]);
+    }
+
 }
