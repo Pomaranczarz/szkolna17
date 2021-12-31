6515df12104a7d4ac5606025580a64033a964812
Pomaranczarz <54837696+Pomaranczarz@users.noreply.github.com>
2021-12-31 11:00:13 +0100

    dodano rozwiązanie do 14.5.c

--- strukturalne/14/14.5.c
+++ strukturalne/14/14.5.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stirng.h>
+
+#define MAX_TITLE_LEN 40
+#define MAX_AUTHOR_LEN 20
 
 struct Book {
-    char title[40];
-    char author[20];
+    char title[MAX_TITLE_LEN];
+    char author[MAX_AUTHOR_LEN];
 };
 
 void ReadStdinToBook(struct Book* book);
 void SaveBookToFile(const char* filename, struct Book book);
-void ReadFileToBook(const char* filename, struct Book* book);
+void ReadFileToBooks(const char* filename, struct Book* books, size_t num_of_books);
+void PrintBookToStdin(struct Book book);
+void PrintBookIf(struct Book* books, size_t num_of_books, bool(*predicate)(struct Book));
+bool IsAuthorNowak(struct Book book) { return strcmp(book.author, "Nowak") == 0; }
 
 int main(void) {
     size_t num_of_books;
@@ -17,10 +25,20 @@ int main(void) {
 
     struct Book* books = (struct Book*)malloc(num_of_books * sizeof(struct Book));
 
-    for (int i = 0; i < num_of_books; i++)
+    for (int i = 0; i < num_of_books; i++) {
         ReadStdinToBook(&books[i]);
+        SaveBookToFile("ksiazki.txt", books[i]);
+    }
 
+    free(books);
+    books = (struct Book*)malloc(num_of_books * sizeof(struct Book));
 
+    ReadFileToBooks("ksiazki.txt", books, num_of_books);
+
+    for (int i = 0; i < num_of_books; i++)
+        PrintBookToStdin(books[i]);
+
+    free(books);
     return 0;
 }
 
@@ -30,16 +48,39 @@ void SaveBookToFile(const char* filename, struct Book book) {
         perror("Nie udalo sie otworzyc pliku");
         return;
     }
+
+    fprintf(out, "%s %s\n", book.title, book.author);
+
+    fclose(out);
 }
 
-void ReadFileToBook(const char* filename, struct Book* book) {
+void ReadFileToBooks(const char* filename, struct Book* books, size_t num_of_books) {
+    FILE* in = fopen(filename, "r");
+    if (in == NULL) {
+        perror("Nie udalo sie otworzyc pliku");
+        return;
+    }
+    
+    for (int i = 0; i < num_of_books; i++)
+        fscanf(in, "%s %s\n", books[i].title, books[i].author);
 
+    fclose(in);
 }
 
 void ReadStdinToBook(struct Book* book) {
-    printf("Podaj tytul (max 40 znakow): \n");
+    printf("Podaj tytul (max %d znakow): \n", MAX_TITLE_LEN);
     scanf("%s", book->title);
 
-    printf("Podaj autora (max 20 znakow): \n");
-    scanf9("%s", book->author);
+    printf("Podaj autora (max %d znakow): \n", MAX_AUTHOR_LEN);
+    scanf("%s", book->author);
+}
+
+void PrintBookToStdin(struct Book book) {
+    printf("Tytul: %s\tAutor: %s\n", book.title, book.author);
+}
+
+void PrintBookIf(struct Book* books, size_t num_of_books, bool(*predicate)(struct Book)) {
+    for (int i = 0; i < num_of_books; i++)
+        if (predicate(books[i]))
+            PrintBookToStdin(books[i]);
 }
