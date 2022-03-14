84f714fd8ebe918a73851542277b01ae01bc2b39
Pomaranczarz <and.cza553@gmail.com>
2022-03-14 14:09:52 +0100

    poprawiono quicksorta

--- II semestr/podstawy algorytmiki/3/quicksort.cpp
+++ II semestr/podstawy algorytmiki/3/quicksort.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 
-void quicksort(int* tab, int size);
+void quicksort(int* tab, int left, int right);
 
 int main() {
+    int tab[] { 6, 3, 4, 8, 9, 7, 1, 2, 5 };
+    quicksort(tab, 0, 8);
+
+    for (int i = 0; i < 9; i++)
+        std::cout << tab[i] << " ";
 
 }
 
-void quickSort(int* tab, int size) {
-    if (size <= 1)
+void quicksort(int* tab, int left, int right) {
+    if (left >= right) // nie ma co sortować
         return;
 
-    int pivot = tab[size / 2];
-    int i = 0;
-    int j = size - 1;
+    int pivot = tab[(left + right) / 2];
+    int i = left;
+    int j = right;
 
     while (i <= j) {
-        while (tab[i] < pivot) i++;
-        while (tab[j] > pivot) j--;
+        while (tab[i] < pivot) ++i;
+        while (tab[j] > pivot) --j;
+
         if (i <= j) {
             std::swap(tab[i], tab[j]);
-            i++;
-            j--;
+            ++i;
+            --j;
         }
     }
-    quickSort(tab, i);
-    quickSort(tab + i, size - i);
+
+    quicksort(tab, left, j);
+    quicksort(tab, i, right);
 }
