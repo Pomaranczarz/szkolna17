cf5f7fe12536bd44b73f343968c83fac1d05dde1
Pomaranczarz <54837696+Pomaranczarz@users.noreply.github.com>
2022-04-04 18:25:23 +0200

    Update karp-rabin.cpp

--- II_semestr/podstawy_algorytmiki/6/karp-rabin.cpp
+++ II_semestr/podstawy_algorytmiki/6/karp-rabin.cpp
@@ -3,30 +3,29 @@
 #include <string>
 #include <cmath>
 
-#define ALPHABET_SIZE 256
+#define ALPHABET_SIZE ('Z' - 'A' + 1) * 2
 
-size_t make_hash(char s, int off, size_t p, size_t exp)
-{
-    return ((int)s - off) * std::pow(p, exp);
+size_t make_hash(char c, int offset, size_t base, size_t pos) {
+    return ((int)c - offset) * std::pow(base, pos);
 }
 
-void update_hash(size_t &hash, char s_prev, char s_next, int off, size_t p, size_t exp)
-{
-    hash -= make_hash(s_prev, off, p, exp);
-    hash *= p;
-    hash += ((int)s_next - off);
+void update_hash(size_t& hash, char s_prev, char s_next, int offset, size_t base, size_t pos) {
+    hash -= make_hash(s_prev, offset, base, pos);
+    hash *= base;
+    hash += ((int)s_next - offset);
 }
 
-void kr(const std::string &pattern, const std::string &text, size_t p, size_t off)
-{
+void kr(const std::string& pattern, const std::string& text, size_t base, size_t offset) {
     size_t hashP = 0;
     size_t hashT = 0;
+
+    bool match = false;
 
     int i = 0;
     while (i < pattern.size())
     {
-        hashP += make_hash(pattern[i], off, p, pattern.size() - 1 - i);
-        hashT += make_hash(text[i], off, p, pattern.size() - 1 - i);
+        hashP += make_hash(pattern[i], offset, base, pattern.size() - 1 - i);
+        hashT += make_hash(text[i], offset, base, pattern.size() - 1 - i);
         ++i;
     }
 
@@ -35,19 +34,23 @@ void kr(const std::string &pattern, const std::string &text, size_t p, size_t of
 
     while (i <= text.size())
     {
-        if (hashP == hashT && pattern == text.substr(j, pattern.size()))
+        if (hashP == hashT) { // && pattern == text.substr(j, pattern.size())
             std::cout << j << " ";
+            match = true;
+        }
 
         if (i < text.size())
-            update_hash(hashT, text[j], text[i], off, p, pattern.size() - 1);
+            update_hash(hashT, text[j], text[i], offset, base, pattern.size() - 1);
 
         ++j;
         ++i;
     }
+
+    if (!match)
+        std::cout << "-1";
 }
 
-int main()
-{
+int main() {
     std::ifstream input{"tekst.txt"};
     if (input.good())
     {
