6a2196be155e4cb16a875cbb31c3c0dd5ad673d4
Pomaranczarz <and.cza553@gmail.com>
2022-04-13 21:11:51 +0200

    Poprawiono implementację kolejki

--- II-semestr/podstawy-algorytmiki/Queue.hpp
+++ II-semestr/podstawy-algorytmiki/Queue.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <stdexcept>
 
 template<typename T>
@@ -8,143 +7,82 @@ class Queue
 private:
     struct Node
     {
-        T value;
-        Node* next;
+        T m_data;
+        Node* m_next;
+
+        Node(const T& data = T{}, Node* next = nullptr)
+            : m_data{ data }
+            , m_next{ next }
+        {
+        }
     };
 public:
-    Queue();
-    ~Queue();
-    void push(const T& value);
-    template<typename... Args>
-    void emplace(Args&&... args);
-    T pop();
-    bool empty();
+    explicit Queue() : m_head{ nullptr }, m_tail{ nullptr }, m_size{ 0 } {}
 
-    T& front();
-    const T& front() const;
+    bool empty() const noexcept {
+        return m_size == 0;
+    }
 
-    T& back();
-    const T& back() const;
+    size_t size() const noexcept {
+        return m_size;
+    }
 
-    size_t size();
+    void push(const T& value) {
+        Node* node = new Node{ value };
+        if (empty()) {
+            m_head = node;
+            m_tail = node;
+        }
+        else {
+            m_tail->m_next = node;
+            m_tail = node;
+        }
 
-    void swap(Queue& other);
+        ++m_size;
+    }
+
+    T pop() {
+        if (empty())
+            throw std::out_of_range("Queue is empty");
+
+        Node* to_delete = m_head;
+        T value = m_head->m_data;
+
+        if (m_head == m_tail) {
+            m_head = nullptr;
+            m_tail = nullptr;
+        }
+        else {
+            m_head = m_head->m_next;
+        }
+
+        delete to_delete;
+        --m_size;
+
+        return value;
+    }
+
+    T front() {
+        if (empty())
+            throw std::out_of_range("Queue is empty");
+
+        return m_head->m_data;
+    }
+
+    T back() {
+        if (empty())
+            throw std::out_of_range("Queue is empty");
+
+        return m_tail->m_data;
+    }
+
+    void clear() {
+        while (!empty())
+            pop();
+    }
 
 private:
-    Node* head;
-    Node* tail;
-    size_t size_{ 0 };
+    Node* m_head;
+    Node* m_tail;
+    size_t m_size;
 };
-
-template<typename T>
-Queue<T>::Queue()
-    : head{ nullptr }
-    , tail{ nullptr }
-{
-    head = nullptr;
-    tail = nullptr;
-}
-
-template<typename T>
-Queue<T>::~Queue() {
-    while (head != nullptr) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-}
-
-template<typename T>
-T Queue<T>::pop() {
-    if (empty())
-        throw std::out_of_range{ "Queue is empty" };
-
-    int value = head->value;
-    Node* temp = head;
-    head = head->next;
-    delete temp;
-
-    --size_;
-
-    return value;
-}
-
-template<typename T>
-bool Queue<T>::empty() {
-    return size_ == 0;
-}
-
-template<typename T>
-void Queue<T>::push(const T& value) {
-    Node* temp = new Node;
-    temp->value = value;
-    temp->next = nullptr;
-    if (empty()) {
-        head = temp;
-        tail = temp;
-    }
-    else {
-        tail->next = temp;
-        tail = temp;
-    }
-
-    ++size_;
-}
-
-template<typename T>
-template<typename... Args>
-void Queue<T>::emplace(Args&&... args) {
-    push(T{ std::forward<Args>(args)... });
-}
-
-template<typename T>
-T& Queue<T>::front() {
-    if (empty())
-        throw std::out_of_range{ "Queue is empty" };
-
-    return head->value;
-}
-
-template<typename T>
-const T& Queue<T>::front() const {
-    if (empty())
-        throw std::out_of_range{ "Queue is empty" };
-
-    return head->value;
-}
-
-template<typename T>
-T& Queue<T>::back() {
-    if (empty())
-        throw std::out_of_range{ "Queue is empty" };
-
-    return tail->value;
-}
-
-template<typename T>
-const T& Queue<T>::back() const {
-    if (empty())
-        throw std::out_of_range{ "Queue is empty" };
-
-    return tail->value;
-}
-
-template<typename T>
-size_t Queue<T>::size() {
-    return size_;
-}
-
-template<typename T>
-void Queue<T>::swap(Queue& other) {
-    std::swap(head, other.head);
-    std::swap(tail, other.tail);
-    std::swap(size_, other.size_);
-}
-
-namespace std
-{
-    template<typename T>
-    void swap(Queue<T>& lhs, Queue<T>& rhs) {
-        lhs.swap(rhs);
-    }
-}
